9c5d7cef84dad89f061f4ad46d389e7c478e0ee4
RiccardoCadenelli <riccardo.cadenelli@gmail.com>
2022-04-01 21:55:51 +0200

    Risistemazione classe Carrello

--- src/main/java/sk/llpc/model/Carrello.java
+++ src/main/java/sk/llpc/model/Carrello.java
@@ -1,31 +1,31 @@
 package sk.llpc.model;
 
+import java.util.Random;
+
 public class Carrello {
     private int nProducts;
 
-    public Carrello(int nP){
-        this.nProducts = nP;
+    public Carrello(){
+        this.nProducts = (int)(Math.random()*100)+ 1;
     }
 
     /**
      * Operation removeProduct
      * Ritorna false se il carrello è vuoto.
      *
-     * @paramn nProducts - Prodotti da togliere
-     * @return boolean
+     * @paramn nProducts - Prodotti contenuti nel carrello
+     * @param q - parametro che indica quanti elementi devo essere estratti dal carrello
+     * @return boolean false se il carrello è vuoto
      */
 
-    public boolean removeProduct(){
-        int temp = this.nProducts;
-        while (temp != 0){
-            nProducts--;
-            break;
-        }
-        if(nProducts < temp){ //nProducts < this.nProducts
-            return true;
+    public boolean removeProduct(int q){
+        this.nProducts -= q;
+
+        if(this.nProducts <= 0){
+            return false;
         }
         else
-            return false;
+            return true;
     }
 }
 
